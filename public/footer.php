7f96d8fe79a8b50863828a0ce4506af98a62f794
bayusky01 <adeyemibayonle28@gmail.com>
2021-11-15 17:55:39 +0100

    Initialized project

---
+++ public/footer.php
@@ -0,0 +1,24 @@
+
+<!--Footer-->
+  <footer id="footer" class="footer">
+    <div class="container text-center">
+
+      ©2021 UI graguating students' clearance
+      <div class="credits">
+        <!--
+          All the links in the footer should remain intact.
+          You can delete the links only if you purchased the pro version.
+          Licensing information: https://bootstrapmade.com/license/
+          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Mentor
+        -->
+      </div>
+    </div>
+  </footer>
+  <!--/ Footer-->
+
+  <script src="../js/jquery.min.js"></script>
+  <script src="../js/jquery.eassssing.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <script src="../js/custom.js"></script>
+  <script src="../contactform/contactform.js"></script>
+
